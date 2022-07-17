afb0aa6cbcdf835b31090a9c3f67308553a813e8
Sachi Joshi <67288962+sachijoshi85320@users.noreply.github.com>
2022-07-18 01:33:25 +0530

    Add files via upload
    Adding two new Codes for Udating database using Update Query and another is the deletion query.

---
+++ deleteQuery.java
@@ -0,0 +1,30 @@
+package com.mycompany.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author HP
+ */
+public class deleteQuery {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //step 1 : load and register drive
+        Class.forName("com.mysql.jdbc.Driver");
+        //step 2: Establish Connection b/w java application and database
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bank1","root","sachijoshiA1@");   
+        //create statement object
+        Statement st=con.createStatement();
+        //send and excute a SQL instruction
+        int rowCount=st.executeUpdate("delete from task where id=12");
+        if(rowCount>0)
+        {
+            System.out.println("Success");
+        }
+        else{
+            System.err.println("Failure");
+        }
+    }
+}
